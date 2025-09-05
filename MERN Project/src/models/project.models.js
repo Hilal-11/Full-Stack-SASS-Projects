785fb9b5e877e09ced814bba39aefeafcb037b46
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-05 15:46:58 +0530

    models

--- MERN Project/src/models/project.models.js
+++ MERN Project/src/models/project.models.js
@@ -1,8 +1,8 @@
 import mongoose, { Schema } from "mongoose";
 const projectSchema = new mongoose.Schema({
 
-    projectName: { type: String, required: true },
-    projectDiscription: { type: String , required: true },
+    projectName: { type: String, required: true, unique: true, trim: true },
+    projectDiscription: { type: String , required: true, trim: true },
     createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true }
 },{
     timestamps: true
