94e087e264c4716fd0e814339ab8f81c0c69ac6d
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-05 15:46:35 +0530

    models

--- MERN Project/src/models/projectmember.models.js
+++ MERN Project/src/models/projectmember.models.js
@@ -1,9 +1,10 @@
 import mongoose, { Schema } from "mongoose";
+import { AvaliableUserRoles , UserRolesEnum } from '../utils/constants'
 const projectmemberSchema = new mongoose.Schema({
 
-    user: { type: String},
+    user: { type: Schema.Types.ObjectId, ref: "User", required: true, },
     project: { type: Schema.Types.ObjectId, ref: "Project" , required: true},
-    role: { }
+    role: { type: String , enum: AvaliableUserRoles , default: UserRolesEnum.MEMBER, required: true}
     
 },{
     timestamps: true
