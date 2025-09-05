fcb4274148361c70bb8a1443be512bbc736d7783
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-05 13:49:53 +0530

    db relations

--- MERN Project/src/models/note.models.js
+++ MERN Project/src/models/note.models.js
@@ -1,8 +1,8 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 const noteSchema = new mongoose.Schema({
+    project: { type: Schema.Types.ObjectId, ref: "Project" , required: true },
 
-},{
-    timestamps: true
-})
+
+},{ timestamps: true })
 
 export default mongoose.model('Note' , noteSchema);
--- MERN Project/src/models/user.models.js
+++ MERN Project/src/models/user.models.js
@@ -1,9 +1,18 @@
 
 import mongoose from 'mongoose'
 const UserSchema = new mongoose.Schema({
+    id: { type: String },
+    avatar: { type: String, required: false },
+    username: { type: String, required: true, unique: true },
+    fullname: { type: String, required: true },
+    password: { type: String, required: true },
+    email: { type: String, required: true },
+    isVarifiedEmail: { type: Boolean, default: false },
+    refreshToken: { type: String },
+    forgetPasswordToken: { type: String },
+    emailVarifyToken: { type: String },
+    emailVarifyTokenExpiry: { type: String }
 
-},{
-    timestamps: true
-})
+},{timestamps: true })
 
 export default mongoose.model("User", UserSchema)
