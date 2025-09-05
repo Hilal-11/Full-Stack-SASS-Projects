8df9a73e7f96f16001c6b4b1f9b3bac393ce62e7
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-05 15:46:22 +0530

    models

--- MERN Project/src/models/subtask.models.js
+++ MERN Project/src/models/subtask.models.js
@@ -1,6 +1,11 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 const subTasksSchema = new mongoose.Schema({
-    
+
+    title: { type: String, required: true, trim: true },
+    task: { type:Schema.Types.ObjectId, ref: "Task", required: true},
+    isCompleted: { type: Boolean, required: true, default: false},
+    createdBy: { type: Schema.Types.ObjectId, ref: "User", required: true}
+
 },{
     timestamps: true
 })
