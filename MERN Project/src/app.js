44150754bd5bd02e09ea84b3dac8ff9cd42af4f7
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-10 17:25:39 +0530

    fuc|******k

--- MERN Project/src/app.js
+++ MERN Project/src/app.js
@@ -1,6 +1,6 @@
 import express from 'express'
 const app = express();
-import healthCheckRouter from './routes/healthcheck.routes';
+import healthCheckRouter from './routes/healthcheck.routes.js';
 
 app.use(express.json());
 app.use("/api/v1" , healthCheckRouter)
