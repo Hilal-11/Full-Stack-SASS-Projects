b0eb43212561189b11a09ae7dd0784bc30f9c088
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-14 02:41:55 +0530

    classical props

--- Next and React/App.js
+++ Next and React/App.js
@@ -1,17 +1,31 @@
 import React from "https://esm.sh/react@19.1.0";
 import ReactDOM from 'https://esm.sh/react-dom@19.1.0/client'
+
+const dynamic = (props) => {
+    console.log(props)
+    return React.createElement(
+        "div",
+        {},
+        [
+            React.createElement("h1" , {} , props.name),
+        ]
+    )
+}
 const App = () => {
    return React.createElement(
     "div",
     {},
-    React.createElement(
-        "h1",
-        {},
-        "Hello this is react Internals"
-    )
+    [
+        React.createElement(dynamic , { name: "Javascript"}),
+        React.createElement(dynamic , { name: "Typescript"}),
+        React.createElement(dynamic , { name: "Python"}),
+        React.createElement(dynamic , { name: "Java"}),
+        React.createElement(dynamic , { name: "C++ and Rust"})
+
+
+    ]
    )
 };
-
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container)
 root.render(React.createElement(App))
