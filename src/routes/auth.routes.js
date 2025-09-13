b1b93d3458cf0e8986a2105a59bb6198320f9b3d
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-13 21:27:17 +0530

    ::))

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,6 +1,10 @@
 import express from 'express'
 const router = express.Router();
+import { registerUser } from '../controllers/auth.controller.js'
+import { validate } from '../middlewares/validator.middleware.js';
+import { userRegistrationValidator } from '../validator/index.js';
 
-
+router.route("/register").
+post(userRegistrationValidator(), validate , registerUser);   // factory pattern
 
 export default router;
