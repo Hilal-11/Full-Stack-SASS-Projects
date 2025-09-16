8f606517d76d6ce6b97cdce44b0297c39667cda9
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-09-16 14:58:30 +0530

    :)

--- tanstack-routing/src/routes/about.jsx
+++ tanstack-routing/src/routes/about.jsx
@@ -1,10 +1,14 @@
 import { createFileRoute } from '@tanstack/react-router'
-
+import { useContext } from 'react'
+import { AppContext } from '../Context/UserContext'
 export const Route = createFileRoute('/about')({
   component: RouteComponent,
   defaultPreload: 'intent',
 })
 
 function RouteComponent() {
-  return <div>Hello "/about"!</div>
+
+  const { loading, setLoading } = useContext(AppContext)
+  
+  return <div>{loading && <div>Hello World</div>}</div>
 }
