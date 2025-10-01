e76fad14f8f73efdc8c3851f62658df75f135c53
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-10-01 12:16:24 +0530

    ...

---
+++ src/controllers/project.controller.js
@@ -0,0 +1,21 @@
+
+
+const getProjects = asyncHandler(async(req , res) => {})
+
+const getProjectsByID = asyncHandler(async(req , res) => {})
+
+const createProject = asyncHandler(async(req , res) => {})
+
+const updateProject = asyncHandler(async(req , res) => {})
+
+const deleteProject = asyncHandler(async(req , res) => {})
+
+const addMemberToProject = asyncHandler(async(req , res) => {})
+
+const getProjectMembers = asyncHandler(async(req , res) => {})
+
+const updateProjectMembers = asyncHandler(async(req , res) => {})
+
+const updateMemberRole = asyncHandler(async(req , res) => {})
+
+const deleteMember = asyncHandler(async(req , res) => {})
