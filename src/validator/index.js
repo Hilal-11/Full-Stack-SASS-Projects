d00eb3dc0b3663f47502f483c86f9ba3e17a5205
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-10-03 16:20:09 +0530

    Auth Validation

--- src/validator/index.js
+++ src/validator/index.js
@@ -28,27 +28,54 @@ const userLoginValidator = () => {
             .trim()
             .notEmpty().withMessage("Email is required")
             .isEmail().withMessage("Email is invalid"),
+        body("username")
+             .optional(),
         body('password')
             .trim()
             .notEmpty().withMessage("Password is required")
     ]
 }
-
+const userChangeCurrentPasswordValidator = () => {
+    return [
+        body("oldPassword")
+            .trim(),
+            notEmpty().withMessage("Old password is required"),
+        body("newPassword")
+            .trim()
+            .notEmpty().withMessage("New password is required"),
+    ]
+}
 const forgetPasswordValidator= () => {
     return [
-
+        body("email")
+        .notEmpty()
+        .withMessage("Email is required")
+        .isEmail()
+        .withMessage("Email is invalid"),
     ]
 }
 
 const resetPasswordValidator = () => {
     return [
-
+        body("newPassword")
+            .notEmpty().withMessage("Password is required")
     ]
 }
+
+const userAssignRoleValidator = () => {
+  return [
+    body("role")
+      .optional()
+      .isIn(AvailableUserRoles)
+      .withMessage("Invalid user role"),
+  ];
+};
 
 export { 
     userRegistrationValidator,
     userLoginValidator,
+    userChangeCurrentPasswordValidator,
+    userAssignRoleValidator,
     forgetPasswordValidator,
     resetPasswordValidator
 }
