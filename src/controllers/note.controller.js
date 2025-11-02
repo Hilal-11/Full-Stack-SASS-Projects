504297e34ef21b4ccd2b8c871436078ef5aad0eb
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-02 23:33:04 +0530

    ...

--- src/controllers/note.controller.js
+++ src/controllers/note.controller.js
@@ -69,18 +69,45 @@ const createNote = asyncHandler(async (req , res) => {
         return res.status(200).json(new ApiResponse(200 , populatedNote ,"Note Created successfully"))
 
     }catch(error) {
+        throw new ApiError(501, "Failed to create the Note")
 
     }
 })
 
 const updateNote = asyncHandler(async (req , res) => {
-    
+    // update note 
+    const { noteId } = req.params;
+    const { content } = req.body;
+    try{
+        const note = await Project.findById(noteId)
+        if(!note) {
+            throw new ApiError(401, "Project not found")
+        }
+
+        const updateNote = await Note.create({
+            content,
+            createdBy: new mongoose.Types.ObjectId(req.user._id)
+        })
+
+    }catch(error) {
+        throw new ApiError(501, "Failed to update the Note")
+    }
 })
 
 const deleteNote = asyncHandler(async (req , res) => {
-    
+    const { projectId } = req.params;
+    const { noteId } = req.body;
+    try{
+        const note = await Project.findByIdAndDelete(noteId)
+        if(!note) {
+            throw new ApiError(401, "Project not found")
+        }
+ 
+        return res.status(200).json(new ApiResponse(200 ,"Note deleted successfully"))
+    }catch(error) {
+        return new ApiError(501, "failed to delete the Note")
+    }
 })
-
 export {
     getNote ,
     getNoteById,
