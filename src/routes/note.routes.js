74b88a867c7b6a01db1d082a9f3cf9565c9e37d6
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-03 00:11:50 +0530

    Update note routes for cleaner structure and middleware integration

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,17 +1,19 @@
 import express from 'express'
 const router = express.Router();
-import { UserRolesEnum } from '../utils/constants'
+import { UserRolesEnum , AvaliableUserRoles } from '../utils/constants'
 import { getNote , getNoteById, createNote, updateNote, deleteNote } from '../controllers/note.controller';
 import { validateProjectPermission } from '../middlewares/auth.middleware';
-
+import { validate } from '../middlewares/validator.middleware';
+import { createNoteValidator, updateNoteValidator , deleteNoteValidator } from '../validator';
 
 router.route("/:projectId")
     .get(validatePermissions([UserRolesEnum.ADMIN , UserRolesEnum.MEMBER]), getNote)
-    .get(validateProjectPermission([UserRolesEnum.ADMIN , UserRolesEnum.MEMBER]), getNoteById)
-    .post(validatePermissions([UserRolesEnum.ADMIN, UserRolesEnum.MEMBER]), createNote)
-    .post(validateProjectPermission([UserRolesEnum.ADMIN , UserRolesEnum.MEMBER]), updateNote)
-    .get(validateProjectPermission([UserRolesEnum.ADMIN , UserRolesEnum.MEMBER]), deleteNote)
+    .post(createNoteValidator(), validate , validatePermissions([UserRolesEnum.ADMIN, UserRolesEnum.MEMBER]), createNote)
 
+router.route("/:projectId/n/:noteId")
+    .get(validateProjectPermission([UserRolesEnum.ADMIN , UserRolesEnum.MEMBER]), getNoteById)
+    .put(updateNoteValidator(), validate , validateProjectPermission([UserRolesEnum.ADMIN , UserRolesEnum.MEMBER]), updateNote)
+    .delete(deleteNoteValidator(), validate , validateProjectPermission([UserRolesEnum.ADMIN , UserRolesEnum.MEMBER]), deleteNote)
 
 //  ---Middleware--- validation for who can access --- Permissions ---
 export default router;
