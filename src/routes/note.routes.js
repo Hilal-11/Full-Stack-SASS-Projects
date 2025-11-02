95e05f996db987932e0830e084c4fffd81aaba33
Hilal-11 <hilalahmadcodedev123@gmail.com>
2025-11-02 23:38:47 +0530

    user roles and note toutes are done

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,14 +1,16 @@
 import express from 'express'
 const router = express.Router();
 import { UserRolesEnum } from '../utils/constants'
+import { getNote , getNoteById, createNote, updateNote, deleteNote } from '../controllers/note.controller';
+import { validateProjectPermission } from '../middlewares/auth.middleware';
 
 
 router.route("/:projectId")
-
-    .get(
-        validatePermissions([UserRolesEnum.ADMIN , UserRolesEnum.MEMBER]), getNotes)
-    .post(
-        validatePermissions([UserRolesEnum.ADMIN, UserRolesEnum.MEMBER]), createNote)
+    .get(validatePermissions([UserRolesEnum.ADMIN , UserRolesEnum.MEMBER]), getNote)
+    .get(validateProjectPermission([UserRolesEnum.ADMIN , UserRolesEnum.MEMBER]), getNoteById)
+    .post(validatePermissions([UserRolesEnum.ADMIN, UserRolesEnum.MEMBER]), createNote)
+    .post(validateProjectPermission([UserRolesEnum.ADMIN , UserRolesEnum.MEMBER]), updateNote)
+    .get(validateProjectPermission([UserRolesEnum.ADMIN , UserRolesEnum.MEMBER]), deleteNote)
 
 
 //  ---Middleware--- validation for who can access --- Permissions ---
